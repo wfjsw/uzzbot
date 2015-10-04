8b6ad10a9d4a5f602c6b2fcd74770a19864f39a0
jsw <l.lllkj@163.com>
2015-10-04 11:03:28 +0800

    add spam unblock

---
+++ plugins/unblock.lua
@@ -0,0 +1,15 @@
+
+local function run(msg, matches)
+  if msg.to.type ~= 'chat' then
+    return "You are unblocked :)"
+  end
+end
+
+return {
+  description = "Unblock me from the limitation of SPAM REPORT!",
+  usage = "!unblock: test whether works",
+  patterns = {
+    "^!unblock$"
+  }, 
+  run = run 
+}
