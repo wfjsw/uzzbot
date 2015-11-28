2581082d735adfd52d5431c35b3a7a37e384fb8d
jsw <l.lllkj@163.com>
2015-11-28 23:40:09 +0800

    bug fix

--- plugins/joing.lua
+++ plugins/joing.lua
@@ -2,6 +2,7 @@ do
 
 function run(msg, matches)
   local target = "chat#id"..tostring(tonumber(matches[1], 16))
+  local user = "user#id"..tostring(msg.from.id)
   join = chat_add_user(target,msg.from.id,ok_cb,false)
 end
 
